1b166d6ade7644a15d1d283c310e935e54a45981
primostasis123 <editomoncada123@gmail.com>
2023-05-23 22:14:17 +0800

    mobile view
    fix view when mobile

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -4,7 +4,21 @@ import { faBriefcase } from '@fortawesome/free-solid-svg-icons'
 export default function Experience() {
     return (
         <div className="lg:rounded-2xl bg-white dark:bg-[#111111]">
-            <div className="pt-12 md:py-12 px-2 sm:px-5 md:px-10 lg:px-14">
+            <div className="pb-12 px-2 sm:px-5 md:px-10 lg:px-14 hide block ">
+                <h3 className="text-[35px] text-white font-bold font-robotoSlab "> May 23, 2023</h3>
+                <div className="lg:grid grid-cols-12 md:gap-10 pt-4 md:pt-[30px] items-center ">
+                    <div className="col-span-12 space-y-2.5">
+                        <div className="lg:mr-16">
+                            <p className="text-[#44566c] text-color-910 leading-7">
+                                {"Hi, as of the moment, this website doesn't have a mobile view experience as it is still under construction. <br/>"}
+                                {"Please switch to desktop view. Thank you for your understanding."}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="pt-12 md:py-12 px-2 sm:px-5 md:px-10 lg:px-14 hidden lg:block">
                 <h2 className="after-effect after:left-52">Experience</h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-x-6 gap-y-6 mt-[30px]">
                     <div className="items-center space-x-2 mb-4">
@@ -14,7 +28,7 @@ export default function Experience() {
                             <p className="text-blue-800 "> <a target="_blank" href="https://ring-group.jp ">https://ring-group.jp  </a></p>
                        </div>
                     </div>
-                    <div className="items-center space-x-2 mb-4">
+                    <div className="items-center space-x-2 mb-4 hidden lg:block">
                        <div className="bg-[#fff4f4] dark:bg-transparent py-4 pl-5 pr-3 space-y-2 mb-6 rounded-lg dark:border-[#212425] dark:border-2">
                             <span className="text-tiny text-gray-lite dark:text-[#b7b7b7]"> March 2022 - Present</span>
                             <h3 className="text-xl text-white"> Eviden (Atos Group)</h3>
@@ -24,7 +38,7 @@ export default function Experience() {
                 </div>
             </div>
 
-            <div className="pb-12 px-2 sm:px-5 md:px-10 lg:px-14">
+            <div className="pb-12 px-2 sm:px-5 md:px-10 lg:px-14 hidden lg:block">
                 <h3 className="text-[35px] text-white font-bold font-robotoSlab "> What I do!</h3>
                 <div className="lg:grid grid-cols-12 md:gap-10 pt-4 md:pt-[30px] items-center hidden">
                     <div className="col-span-12 space-y-2.5">
